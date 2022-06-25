fc5b843d8dbdd15fc65ce2dafad573b0b16a310d
Ming Chang <mail@mingchang.tw>
2022-06-25 16:25:45 +0800

    message: remove Validator.

--- message-service/src/main/java/com/escambia/official/messageservice/MessageServiceApplication.java
+++ message-service/src/main/java/com/escambia/official/messageservice/MessageServiceApplication.java
@@ -3,14 +3,10 @@ package com.escambia.official.messageservice;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Validator;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -44,9 +40,5 @@ public class MessageServiceApplication {
                                             |___/                                        \s
                 """;
     }
-
-    @Bean
-    public ValidatingMongoEventListener validatingMongoEventListener(Validator validator) {
-        return new ValidatingMongoEventListener(validator);
-    }
+    
 }
