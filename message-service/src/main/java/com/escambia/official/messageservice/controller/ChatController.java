6492973da6b100af9ed3b19f7a48c84cb7d14170
Ming Chang <mail@mingchang.tw>
2022-06-25 20:10:35 +0800

    message: add swagger api summary.

--- message-service/src/main/java/com/escambia/official/messageservice/controller/ChatController.java
+++ message-service/src/main/java/com/escambia/official/messageservice/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.escambia.official.messageservice.controller;
 
 import com.escambia.official.messageservice.model.Chat;
 import com.escambia.official.messageservice.service.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -19,16 +20,19 @@ public class ChatController {
         this.chatService = chatService;
     }
 
+    @Operation(summary = "查詢所有聊天室記錄")
     @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Chat> getAllChat(Integer chatRoomId) {
         return chatService.getAllChat(chatRoomId);
     }
 
+    @Operation(summary = "傳送訊息")
     @PostMapping(value = "/send", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Chat> sendChat(@RequestBody Chat chat, String apnsToken) {
         return chatService.sendChat(chat, apnsToken);
     }
 
+    @Operation(summary = "接收訊息（Server Sent Event）")
     @GetMapping(value = "/openStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> openStream(Integer chatRoomId, Integer userId) {
         return chatService.openStream(chatRoomId, userId);
